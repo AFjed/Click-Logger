9e40707600e8d4260ed4f9c1eed128a9c169be2d
AFjed <141450135+AFjed@users.noreply.github.com>
2024-03-17 12:25:15 -0500

    Update click_logger.py

--- click_logger.py
+++ click_logger.py
@@ -2,10 +2,8 @@ import os
 import webbrowser
 
 if __name__ == "__main__":
-    #https://AFjed.github.io/Click-Logger/redirect.html
-
     redirect_url = 'https://AFjed.github.io/Click-Logger/redirect.html'
-    webbrowser.open(redirect_url)
+    webbrowser.open_new_tab(redirect_url)
 
     # Exit the script
     os._exit(0)
