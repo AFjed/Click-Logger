16fd401170a197f6fe33240a5d32323f6ecb81e6
AFjed <141450135+AFjed@users.noreply.github.com>
2024-03-17 12:09:38 -0500

    Update click_logger.py

--- click_logger.py
+++ click_logger.py
@@ -1,8 +1,8 @@
 import requests
 
-def log_click(user_id):
+def log_click(url, user_id):
     webhook_url = 'https://discord.com/api/webhooks/1218947479999877151/nloZbM1-6HP8SZkD1illqnEl_vvPUOA2FHXH0hPy9YzJJ-Qec0SSO9MYXeBye7InhQW3'
-    payload = {'content': f'User {user_id} clicked the link.'}
+    payload = {'content': f'User {user_id} clicked the link: {url}'}
     response = requests.post(webhook_url, json=payload)
     if response.status_code == 200:
         print('Successfully logged click.')
@@ -10,5 +10,7 @@ def log_click(user_id):
         print(f'Failed to log click. Status code: {response.status_code}')
 
 if __name__ == "__main__":
+    url = input("Enter the URL you want to track:https://example.com ")
     user_id = input("Enter user ID: ")
-    log_click(user_id)  # For Windows
+    log_click(url, user_id)
+
