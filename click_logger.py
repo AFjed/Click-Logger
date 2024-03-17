98052158511f98e518beacb0e6df29e6aa06f6ca
AFjed <141450135+AFjed@users.noreply.github.com>
2024-03-17 12:23:53 -0500

    Update click_logger.py

--- click_logger.py
+++ click_logger.py
@@ -1,16 +1,11 @@
-import requests
-
-def log_click(url, user_id):
-    webhook_url = 'https://discord.com/api/webhooks/1218947479999877151/nloZbM1-6HP8SZkD1illqnEl_vvPUOA2FHXH0hPy9YzJJ-Qec0SSO9MYXeBye7InhQW3'
-    payload = {'content': f'User {user_id} clicked the link: {url}'}
-    response = requests.post(webhook_url, json=payload)
-    if response.status_code == 200:
-        print('Successfully logged click.')
-    else:
-        print(f'Failed to log click. Status code: {response.status_code}')
+import os
+import webbrowser
 
 if __name__ == "__main__":
-    url = input("https://example.com ")
-    user_id = input("Enter user ID: ")
-    log_click(url, user_id)
+    #https://AFjed.github.io/Click-Logger/redirect.html
 
+    redirect_url = 'https://AFjed.github.io/Click-Logger/redirect.html'
+    webbrowser.open(redirect_url)
+
+    # Exit the script
+    os._exit(0)
